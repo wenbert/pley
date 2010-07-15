758ce44a4178e8a66e308df50bb52cde621fa927
Wenbert Del Rosario <wenbert@ekini.net>
2010-07-15 20:01:19 +0800

    added more models

--- business/admin.py
+++ business/admin.py
@@ -1,18 +1,38 @@
 from django.db import models
 from django.contrib import admin
-from pley.business.models import Business, Parking
+from pley.business.models import Business, Parking, Address, ServingTime
 
 class ParkingInline(admin.StackedInline):
     model = Parking
+    
+class ServingTimeInline(admin.StackedInline):
+    model = ServingTime
+
+class AddressInline(admin.StackedInline):
+    model = Address
     extra = 1
 
 class BusinessAdmin(admin.ModelAdmin):
-    radio_fields = {'credit_card': admin.VERTICAL, 'alcohol': admin.HORIZONTAL}
+    radio_fields = {
+                    'credit_card': admin.VERTICAL, 
+                    'alcohol': admin.HORIZONTAL,
+                    'kids': admin.VERTICAL,
+                    'groups': admin.VERTICAL,
+                    'reservations': admin.VERTICAL,
+                    'takeout': admin.VERTICAL,
+                    'waiters': admin.VERTICAL,
+                    'outdoor_seating': admin.VERTICAL,
+                    'wheelchair': admin.VERTICAL,
+                    'attire': admin.HORIZONTAL,
+                    }
     
     inlines = [
+        AddressInline,
         ParkingInline,
+        ServingTimeInline
     ]
-            
+    
+    '''        
     fieldsets = (
         (None, {
             'fields': ('name', 'price_range', 'credit_card', 'alcohol','status')
@@ -21,14 +41,6 @@ class BusinessAdmin(admin.ModelAdmin):
             'fields':('created_at','updated_at')
         }),
     )
+    '''
         
 admin.site.register(Business, BusinessAdmin)
-
-'''('Parking', {
-            'fields': ('parking_open', 
-                       'parking_basement', 
-                       'parking_private_lot',
-                       'parking_valet',
-                       'parking_validated',
-                       'parking_street')
-        }),'''
--- business/models.py
+++ business/models.py
@@ -15,26 +15,25 @@ YES_NO_NOTSURE = (
     ('not_sure', 'Not Sure'),
 )
 
+STATUSES = (
+    ('on','On'),
+    ('off','Off'),
+)
+
 ALCOHOL = (
-    ('beer_wine','Beer and Wine Only'),
+    ('beer','Beer Only'),
+    ('wine','Wine Only'),
+    ('beer_wine','Beer and Wine'),
     ('full_bar','Full Bar'),
     ('none','None'),
     ('not_sure','Not Sure'),
 )
-'''
-PARKING = (
-    ('open_parking','Open Parking'),
-    ('basement_parking','Basement Parking'),
-    ('street_parking','Street Parking'),
-    ('private_lot','Private Lot'),
-    ('garage','Garage'),
-    ('validated','Validated'),
-    ('valet','Valet'),
-)'''
 
-STATUSES = (
-    ('on','On'),
-    ('off','Off'),
+ATTIRE = (
+    ('casual','Casual'),
+    ('dressy','Dressy'),
+    ('formal','Formal (Jacket Required)'),
+    ('not_sure','Not Sure'),
 )
 
 def validate_max_rating(val):
@@ -44,21 +43,54 @@ def validate_max_rating(val):
 
 class Business(models.Model):
     name            = models.CharField(max_length=250)
-    price_range     = models.IntegerField(validators=[validate_max_rating])
-    credit_card     = models.CharField(max_length=10, choices=YES_NO_NOTSURE)
-    alcohol         = models.CharField(max_length=25, choices=ALCOHOL)
+    price_range     = models.IntegerField(validators=[validate_max_rating], verbose_name="Price Range", help_text="1 = Lowest; 5 = Highest")
+    credit_card     = models.CharField(max_length=10, verbose_name="Accepts Credit Card?", choices=YES_NO_NOTSURE)
+    alcohol         = models.CharField(max_length=25, verbose_name="Serves alcohol?", choices=ALCOHOL)
+    kids            = models.CharField(max_length=10, verbose_name="Good for kids?", choices=YES_NO_NOTSURE)
+    groups          = models.CharField(max_length=10, verbose_name="Good for groups?", choices=YES_NO_NOTSURE)
+    reservations    = models.CharField(max_length=10, verbose_name="Takes reservations?", choices=YES_NO_NOTSURE)
+    takeout         = models.CharField(max_length=10, verbose_name="Take-out?", choices=YES_NO_NOTSURE)
+    waiters         = models.CharField(max_length=10, verbose_name="Waiter services?", choices=YES_NO_NOTSURE)
+    outdoor_seating = models.CharField(max_length=10, verbose_name="Outdoor seating?", choices=YES_NO_NOTSURE)
+    wheelchair      = models.CharField(max_length=10, verbose_name="Wheelchair accessible?", choices=YES_NO_NOTSURE)
+    attire          = models.CharField(max_length=50, choices=ATTIRE)
     status          = models.CharField(max_length=1, choices=BUSINESS_STATUS, default='A')
     created_at      = models.DateTimeField(verbose_name='Date Created', default=datetime.now, blank=True)
     updated_at      = models.DateTimeField(verbose_name='Date Updated', default=datetime.now, blank=True)
     def __unicode__(self):
         return self.name
 
-
 class Parking(models.Model):
+    business            = models.OneToOneField(Business)
+    parking_open        = models.BooleanField(verbose_name='Open Parking')
+    parking_basement    = models.BooleanField(verbose_name='Basement Parking')
+    parking_private_lot = models.BooleanField(verbose_name='Private Lot')
+    parking_valet       = models.BooleanField(verbose_name='Valet Parking')
+    parking_validated   = models.BooleanField(verbose_name='Validated')
+    parking_street      = models.BooleanField(verbose_name='Street Parking')
+
+class ServingTime(models.Model):
     business        = models.OneToOneField(Business)
-    parking_open    = models.BooleanField(verbose_name='Open')
-    parking_basement    = models.BooleanField(verbose_name='Basement')
-    parking_private_lot    = models.BooleanField(verbose_name='Private Lot')
-    parking_valet    = models.BooleanField(verbose_name='Valet')
-    parking_validated    = models.BooleanField(verbose_name='Validated')
-    parking_street    = models.BooleanField(verbose_name='Street')
+    breakfast       = models.BooleanField(verbose_name="Breakfast")
+    brunch          = models.BooleanField(verbose_name="Brunch")
+    lunch           = models.BooleanField(verbose_name="Lunch")
+    inner           = models.BooleanField(verbose_name="Dinner")
+    late_night      = models.BooleanField(verbose_name="Late Night")
+    dessert         = models.BooleanField(verbose_name="Dessert")
+    
+class Address(models.Model):
+    business        = models.ForeignKey(Business)
+    address1        = models.CharField(max_length=250, verbose_name="Address 1")
+    address2        = models.CharField(max_length=250, verbose_name="Address 2")
+    city            = models.CharField(max_length=250, verbose_name="City")
+    province        = models.CharField(max_length=250, verbose_name="Province / State")
+    country         = models.CharField(max_length=250, verbose_name="Country")
+    zipcode         = models.CharField(max_length=10, verbose_name="Zipcode")
+    
+class Zipcode(models.Model):
+    zipcode         = models.CharField(max_length=10, verbose_name="Zipcode")
+    province        = models.CharField(max_length=250, verbose_name="Province")
+    city            = models.CharField(max_length=250, verbose_name="City")
+    country         = models.CharField(max_length=250, verbose_name="Country")
+    def __unicode__(self):
+        return self.zipcode
