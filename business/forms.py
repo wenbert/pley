31af3cc15ea6e5ddd557f1dc102cbfd20c044c66
john2x <john2x@gmail.com>
2010-07-16 11:35:21 +0800

    Added business_form.html template. Added forms. Forms cannot save yet.

--- business/forms.py
+++ business/forms.py
@@ -1,1 +1,24 @@
 from django import forms
+
+from pley.business.models import *
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('created_at', 'updated_at', 'status', )
+
+class ParkingForm(forms.ModelForm):
+    class Meta:
+        model = Parking
+        exclude = ('business',)
+
+class ServingTimeForm(forms.ModelForm):
+    class Meta:
+        model = ServingTime
+        exclude = ('business',)
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        exclude = ('business',)
+
