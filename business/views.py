21dd2d20242a1a0b99865eca629d9acdc046dc13
Wenbert Del Rosario <wenbert@ekini.net>
2010-07-18 10:35:55 +0800

    added business_browse with pagination and moves business templates to a folder

--- business/views.py
+++ business/views.py
@@ -1,55 +1,82 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context, loader
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
-
+from django.db import transaction
+from django.db import IntegrityError
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from pley.business.models import *
 from pley.business.forms import *
 
+def business_browse(request):
+    business_list = Business.objects.all().order_by('-created_at')
+    paginator = Paginator(business_list, 3)
+    
+    try:
+        page = int(request.GET.get('page','1'))
+    except ValueError:
+        page = 1
+    
+    try:
+        businesses = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        businesses = paginator.page(paginator.num_page)
+        
+    #output = ', '.join([b.name for b in businesses])
+    data = {"string": "value",
+            "business_list": business_list,
+            "businesses": businesses
+            }
+    return render_to_response("business/business_browse.html",
+                              data, context_instance=RequestContext(request))
+
+@transaction.commit_manually
 def business_add(request):
     success = False
+    error = False
     if request.method == 'POST':
-        business_form = BusinessForm(request.POST)
-        address_form = AddressForm(request.POST)
-        parking_form = ParkingForm(request.POST)
-        serving_time_form = ServingTimeForm(request.POST)
+        business_form       = BusinessForm(request.POST)
+        address_form        = AddressForm(request.POST)
+        parking_form        = ParkingForm(request.POST)
+        serving_time_form   = ServingTimeForm(request.POST)
 
         if (business_form.is_valid() and address_form.is_valid()
         and parking_form.is_valid() and serving_time_form.is_valid()):
-            business_name = business_form.cleaned_data['name']
-            price_range = business_form.cleaned_data['price_range']
-            credit_card = business_form.cleaned_data['credit_card']
-            alcohol = business_form.cleaned_data['alcohol']
-            kids = business_form.cleaned_data['kids']
-            groups = business_form.cleaned_data['groups']
-            reservations = business_form.cleaned_data['reservations']
-            takeout = business_form.cleaned_data['takeout']
-            waiters = business_form.cleaned_data['waiters']
+            business_name   = business_form.cleaned_data['name']
+            price_range     = business_form.cleaned_data['price_range']
+            credit_card     = business_form.cleaned_data['credit_card']
+            alcohol         = business_form.cleaned_data['alcohol']
+            kids            = business_form.cleaned_data['kids']
+            groups          = business_form.cleaned_data['groups']
+            reservations    = business_form.cleaned_data['reservations']
+            takeout         = business_form.cleaned_data['takeout']
+            waiters         = business_form.cleaned_data['waiters']
             outdoor_seating = business_form.cleaned_data['outdoor_seating']
-            wheelchair = business_form.cleaned_data['wheelchair']
-            attire = business_form.cleaned_data['attire']
+            wheelchair      = business_form.cleaned_data['wheelchair']
+            attire          = business_form.cleaned_data['attire']
 
-            parking_open = parking_form.cleaned_data['parking_open']
-            parking_basement = parking_form.cleaned_data['parking_basement']
+            parking_open        = parking_form.cleaned_data['parking_open']
+            parking_basement    = parking_form.cleaned_data['parking_basement']
             parking_private_lot = parking_form.cleaned_data['parking_private_lot']
-            parking_valet = parking_form.cleaned_data['parking_valet']
-            parking_validated = parking_form.cleaned_data['parking_validated']
-            parking_street = parking_form.cleaned_data['parking_street']
+            parking_valet       = parking_form.cleaned_data['parking_valet']
+            parking_validated   = parking_form.cleaned_data['parking_validated']
+            parking_street      = parking_form.cleaned_data['parking_street']
 
-            address_1 = address_form.cleaned_data['address1']
-            address_2 = address_form.cleaned_data['address2']
-            address_city = address_form.cleaned_data['city']
+            address_1       = address_form.cleaned_data['address1']
+            address_2       = address_form.cleaned_data['address2']
+            address_city    = address_form.cleaned_data['city']
             address_province = address_form.cleaned_data['province']
             address_country = address_form.cleaned_data['country']
             # TODO: zipcode should be found in zipcode table
             address_zipcode = address_form.cleaned_data['zipcode']
 
-            serving_breakfast = serving_time_form.cleaned_data['breakfast']
-            serving_brunch = serving_time_form.cleaned_data['brunch']
-            serving_lunch = serving_time_form.cleaned_data['lunch']
-            serving_dinner = serving_time_form.cleaned_data['dinner']
-            serving_late_night = serving_time_form.cleaned_data['late_night']
-            serving_dessert = serving_time_form.cleaned_data['dessert']
+            serving_breakfast   = serving_time_form.cleaned_data['breakfast']
+            serving_brunch      = serving_time_form.cleaned_data['brunch']
+            serving_lunch       = serving_time_form.cleaned_data['lunch']
+            serving_dinner      = serving_time_form.cleaned_data['dinner']
+            serving_late_night  = serving_time_form.cleaned_data['late_night']
+            serving_dessert     = serving_time_form.cleaned_data['dessert']
 
             # TODO: catch possible exceptions here
             business = Business(name=business_name, price_range=price_range,
@@ -59,30 +86,39 @@ def business_add(request):
                                 outdoor_seating=outdoor_seating,
                                 attire=attire,
                                 wheelchair=wheelchair)
-            business.save()
-            parking = Parking(parking_open=parking_open, 
-                              parking_basement=parking_basement,
-                              parking_private_lot=parking_private_lot,
-                              parking_valet=parking_valet,
-                              parking_validated=parking_validated,
-                              parking_street=parking_street, 
-                              business=business)
-            address = Address(address1=address_1, address2=address_2,
-                              city=address_city, province=address_province,
-                              country=address_country,
-                              zipcode=address_zipcode,
-                              business=business)
-            serving_time = ServingTime(breakfast=serving_breakfast,
-                                       brunch=serving_brunch,
-                                       lunch=serving_lunch,
-                                       dinner=serving_dinner,
-                                       late_night=serving_late_night,
-                                       dessert=serving_dessert, 
-                                       business=business)
-            parking.save()
-            address.save()
-            serving_time.save()
-            success = True
+            try:
+                business.save()
+                parking = Parking(parking_open=parking_open, 
+                                  parking_basement=parking_basement,
+                                  parking_private_lot=parking_private_lot,
+                                  parking_valet=parking_valet,
+                                  parking_validated=parking_validated,
+                                  parking_street=parking_street, 
+                                  business=business)
+                                  
+                address = Address(address1=address_1, address2=address_2,
+                                  city=address_city, province=address_province,
+                                  country=address_country,
+                                  zipcode=address_zipcode,
+                                  business=business)
+                                  
+                serving_time = ServingTime(breakfast=serving_breakfast,
+                                           brunch=serving_brunch,
+                                           lunch=serving_lunch,
+                                           dinner=serving_dinner,
+                                           late_night=serving_late_night,
+                                           dessert=serving_dessert, 
+                                           business=business)
+                parking.save()
+                address.save()
+                serving_time.save()
+            except IntegrityError, e:
+                transaction.rollback()
+                success = False
+                error = e
+            else:
+                transaction.commit()
+                success = True
             #redirect to success page
     else:
         business_form = BusinessForm()
@@ -95,8 +131,9 @@ def business_add(request):
               "address_form": address_form,
               "parking_form": parking_form,
               "serving_time_form": serving_time_form,
-              "success": success
+              "success": success,
+              "error": error
            }
-    return render_to_response("business_form.html",
+    return render_to_response("business/business_add.html",
                               data, context_instance=RequestContext(request))
 
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.create_update import create_object
+from django.views.generic import list_detail
 
 from pley.business.models import Business, Parking
 from pley.business.views import *
@@ -7,6 +8,13 @@ from pley.business.views import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+#to be used in generic views
+business_info = {
+    "queryset": Business.objects.all(),
+    "template_name": "business/business_browse.html",
+    "paginate_by": 3,
+}
 
 urlpatterns = patterns('',
     # Example:
@@ -19,4 +27,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     ('^business/add/$', business_add),
+    ('^business/browse/$', business_browse),
+    #(r'^business/browse/$', list_detail.object_list, business_info),
+    #(r'^business/browse/page(?P<page>[0-9]+/$)',list_detail.object_list, business_info),
 )
