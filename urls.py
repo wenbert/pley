14f7282bfeb5636980dba6e266d924de5960048d
Wenbert Del Rosario <wenbert@ekini.net>
2010-07-18 10:43:46 +0800

    cleaned up urls.py

--- urls.py
+++ urls.py
@@ -10,24 +10,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 #to be used in generic views
-business_info = {
-    "queryset": Business.objects.all(),
-    "template_name": "business/business_browse.html",
-    "paginate_by": 3,
-}
+#business_info = {
+#    "queryset": Business.objects.all(),
+#    "template_name": "business/business_browse.html",
+#    "paginate_by": 3,
+#}
 
 urlpatterns = patterns('',
     # Example:
     # (r'^pley/', include('pley.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    ('^business/add/$', business_add),
-    ('^business/browse/$', business_browse),
+    (r'^business/add/$', business_add),
+    (r'^business/browse/$', business_browse),
     #(r'^business/browse/$', list_detail.object_list, business_info),
     #(r'^business/browse/page(?P<page>[0-9]+/$)',list_detail.object_list, business_info),
 )
