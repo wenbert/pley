d7297755c594d76f68ed625d14f9c2dc9e23abfc
Wenbert Del Rosario <wenbert@ekini.net>
2010-08-18 12:49:44 +0800

    merged conflicts in forms.py

--- business/forms.py
+++ business/forms.py
@@ -7,19 +7,16 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ('created_at', 'updated_at', 'status', 
-<<<<<<< HEAD
-                   'num_reviews', 'rating',)
+                   'num_reviews', 'rating','properties')
         widgets = {
             'name': TextInput(attrs={'class': 'required', 'minlength':'2'}),
             'website': TextInput(attrs={'class': 'url', 'minlength':'2'}),
         }
-=======
-                   'num_reviews', 'rating','properties')
+  
 
 class PropertiesForm(forms.ModelForm):
     class Meta:
         model = Properties
->>>>>>> fa9a8eb126e8bcb668b569b1e3269804f07ebe8f
 
 class BusinessCategoryForm(forms.ModelForm):
     class Meta:
